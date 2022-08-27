fff88736713ddb58232bc5f45e1122389a2ec9cc
jordanfang <fangxd@cn.ibm.com>
2022-08-27 11:00:37 +0800

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -16,7 +16,7 @@ job('NodeJS Docker example') {
         dockerBuildAndPublish {
             repositoryName('jordanfang/docker-nodejs-demo')
             tag('${GIT_REVISION,length=9}')
-            registryCredentials('dockerhub')
+            registryCredentials('Jenkins')
             forcePull(false)
             forceTag(false)
             createFingerprints(false)
